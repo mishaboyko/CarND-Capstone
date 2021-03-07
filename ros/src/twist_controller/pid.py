92a6f4bf2c202cf80bab9eeb84406f106c21a671
Michael Boiko <michael.boiko@bmw.de>
2021-03-07 22:54:47 +0100

    Fix typos and add meaninful description

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -1,3 +1,4 @@
+import rospy
 
 MIN_NUM = float('-inf')
 MAX_NUM = float('inf')
@@ -5,23 +6,30 @@ MAX_NUM = float('inf')
 
 class PID(object):
     def __init__(self, kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM):
+        # Proportional coefficient
         self.kp = kp
+        # Integral coefficient
         self.ki = ki
+        # Differential coefficient
         self.kd = kd
         self.min = mn
         self.max = mx
 
         self.int_val = self.last_error = 0.
+        rospy.loginfo("PID successfully initialized")
 
     def reset(self):
         self.int_val = 0.0
 
     def step(self, error, sample_time):
+        # error is an Cross-Track-Error
+        # sample_time is a current timestamp ?!
 
-        integral = self.int_val + error * sample_time;
-        derivative = (error - self.last_error) / sample_time;
+        integral = self.int_val + error * sample_time
+        derivative = (error - self.last_error) / sample_time
 
-        val = self.kp * error + self.ki * integral + self.kd * derivative;
+        # val is a steering angle ?!
+        val = self.kp * error + self.ki * integral + self.kd * derivative
 
         if val > self.max:
             val = self.max
